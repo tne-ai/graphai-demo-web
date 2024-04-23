e93b51e66a09fdfe143f5c0ba10457a7751b607e
isamu <isamu@to-kyo.to>
2024-04-23 09:40:22 +0900

    update config

--- src/config/project.ts
+++ src/config/project.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAd-pX35BsCvIBDXstf_LmQaz82PNFGcMI",
-  authDomain: "fir-vue-startup-kit.firebaseapp.com",
-  databaseURL: "https://fir-vue-startup-kit.firebaseio.com",
-  projectId: "fir-vue-startup-kit",
-  storageBucket: "fir-vue-startup-kit.appspot.com",
-  messagingSenderId: "225404938985",
-  appId: "1:225404938985:web:a2cea9fab1722fce",
-  messageKey: "BGwIKr3yZdXmg4Pl-BpIQX_ETtHYl0RqvAJFoc-hYKFyaoU3wZXarqto5DSq1cemFuARMjFvJ02B83T5ga42MKo",
+  apiKey: "AIzaSyAy3KXWSUu1CHJEl7O7RNa-1gRF37deWPc",
+  authDomain: "graphai-demo.firebaseapp.com",
+  projectId: "graphai-demo",
+  storageBucket: "graphai-demo.appspot.com",
+  messagingSenderId: "226250365313",
+  appId: "1:226250365313:web:893ac98a6fc95b8418b080",
+  measurementId: "G-0QLMJFML5C"
 };
