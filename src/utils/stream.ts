625dd7237af86b1f4f670dcd9c6d656cddaba20d
isamu <isamu@to-kyo.to>
2024-05-06 10:41:28 +0900

    more test

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -1,5 +1,6 @@
 import { ref, Ref } from "vue";
 import { AgentFilterFunction } from "graphai/lib/type";
+import { randomInt } from "./graph";
 
 export const useStreamData = () => {
   const streamData: Ref<Record<string, string>> = ref({});
@@ -34,46 +35,73 @@ class WordStreamer {
         if (word) {
           next();
         }
-      }, 500);
+      }, randomInt(800));
     };
     next();
   }
 }
 
-export const useGraphData = (theMessage: string) => {
-  const words = ref(new Array<string>());
+export const useGraphData = () => {
+  const words = ref<Record<string, string[]>>({});
 
-  const faucat = (streamer: WordStreamer) => {
-    return new Promise((resolve) => {
-      streamer.onWord = (word: string | undefined) => {
-        if (word) {
-          words.value.push(word);
-        } else {
-          resolve(words.value.join(" "));
-        }
-      };
-    });
+  const faucatGenerator = (key: string) => {
+    words.value[key] = [];
+    return (streamer: WordStreamer) => {
+      return new Promise((resolve) => {
+        streamer.onWord = (word: string | undefined) => {
+          if (word) {
+            words.value[key].push(word);
+          } else {
+            resolve(words.value[key].join(" "));
+          }
+        };
+      });
+    };
   };
+
+  const messages = [
+    "May the force be with you.",
+    "You must unlearn what you have learned.",
+    "No. Try not. Do or do not. There is no try.",
+    "Named must your fear be before banish it, you can.",
+    "Already know you, that which you need.",
+    "When you look at the dark side, careful you must be … for the dark side looks back.",
+    "Train yourself to let go of everything you fear to lose.",
+    "Patience you must have my young padawan.",
+    "Much to learn you still have…my old padawan.",
+    "Always in motion is the future.",
+    "Always two there are, no more, no less. A master and an apprentice.",
+    "Use your feelings, Obi-Wan, and find him you will.",
+    "I cannot teach him. The boy has no patience.",
+    "The dark side clouds everything. Impossible to see the future is.",
+    "A Jedi’s strength flows from the Force.",
+    "Hmm. In the end, cowards are those who follow the dark side.",
+  ];
 
   const graphdata_any = {
     version: 0.2,
     nodes: {
       message: {
-        value: theMessage,
-      },
-      source: {
-        agent: (message: string) => {
-          return new WordStreamer(message);
-        },
-        inputs: ["message"],
-      },
-      destination: {
-        agent: faucat,
-        isResult: true,
-        inputs: ["source"],
+        value: messages,
       },
     },
   };
+
+  Array.from(messages.keys()).forEach((k) => {
+    const message = messages[k];
+    graphdata_any.nodes["source" + k] = {
+      agent: (message: string) => {
+        return new WordStreamer(message);
+      },
+      inputs: ["message.$" + k],
+    };
+    graphdata_any.nodes["destination" + k] = {
+      agent: faucatGenerator("message" + k),
+      isResult: true,
+      inputs: ["source" + k],
+    };
+  });
+
   return {
     graphdata_any,
     words,
